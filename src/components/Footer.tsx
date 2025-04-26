351d13d672b6d6a34fc9870413157b612aad8f99
D8GER <xyb5to0zcy@gmail.com>
2025-04-26 16:27:52 +0800

    feat(clarity): add microsoft clarity
    https://clarity.microsoft.com/

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 export function Footer() {
   return (
     <footer className="bg-gray-50 dark:bg-gray-950 border-t border-slate-200 dark:border-slate-700 py-6 mt-12">
@@ -6,6 +8,16 @@ export function Footer() {
           <p>Copyright &copy; {new Date().getFullYear()} 巽川·怀因 All rights reserved.</p>
         </div>
       </div>
+      {/* microsoft clarity */}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "ra3fjoa5rz");
+        `}
+      </Script>
     </footer>
   );
 }
